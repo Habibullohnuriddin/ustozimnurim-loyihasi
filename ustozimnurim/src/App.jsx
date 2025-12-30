4bbc0fed10559f2d11f6eedc7973b0cf04d01f83
dasturchihabib <nuriddinovx2@gmail.com>
2025-12-30 10:29:55 +0500

    added path location for deploy to versel

--- ustozimnurim/src/App.jsx
+++ ustozimnurim/src/App.jsx
@@ -5,17 +5,15 @@ export default function App() {
     <div className="min-h-screen bg-gray-50 flex items-center justify-center p-6">
       <div className="max-w-2xl w-full bg-white rounded-xl shadow-md p-8">
         <h1 className="text-3xl font-bold text-center text-sky-600">
-          UstozimNurim
+          ustozim nurim loyihasi, tez orada!
         </h1>
-        <p className="mt-4 text-gray-600 text-center">
-          React + Vite + Tailwind starter
-        </p>
+        <p className="mt-4 text-gray-600 text-center"></p>
         <div className="mt-6 flex justify-center">
           <a
-            className="px-4 py-2 bg-sky-500 text-white rounded hover:bg-sky-600"
+            className="px-4 py-2 bg-sky-500 text-white rounded hover:bg-sky-600 "
             href="#"
           >
-            Boshlash
+            nicemi 😊
           </a>
         </div>
       </div>
